25e37852aa95d71827042abe75c3cda84860e65d
roganOh <fdscbjdcnhd@gmail.com>
2023-01-06 05:43:06 +0900

    merge migration script with shell

--- src/test/resources/script/create_test_db_script.sh
+++ src/test/resources/script/create_test_db_script.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
-set -e
+APP_MIGRATION_SCRIPT_NAME=app_migration.sql
+APP_MIGRATION_SCRIPT=../db/${APP_MIGRATION_SCRIPT_NAME}
 
-if [ -z "$1" ]
-then
-  echo "\$1 (docker.mysql.app.init.d) path is NULL"
-  exit 1
+A=`ls ../../../main/resources/db/migration | wc -l`
+
+if [ ${A} -eq 0 ]; then
+  echo "----------------------------------------------------"
+  echo "Migration Script is Null!"
+  echo "----------------------------------------------------"
+  exit 0
 fi
+
+if [ -f $APP_MIGRATION_SCRIPT ]; then
+  rm -f $APP_MIGRATION_SCRIPT
+  echo "Delete Before Migration Script"
+fi
+
+FILE_LIST=`ls ../../../main/resources/db/migration/*`
+
+for item in ${FILE_LIST[@]}
+do
+  cat $item >> $APP_MIGRATION_SCRIPT
+  echo "" >> $APP_MIGRATION_SCRIPT
+  echo "" >> $APP_MIGRATION_SCRIPT
+done
